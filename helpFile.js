869bdb63cb6e0770af632d697d5a9d5c3388b70d
incarnate87 <59734746+incarnate87@users.noreply.github.com>
2020-07-20 23:04:10 +0530

    Update helpFile.js

--- helpFile.js
+++ helpFile.js
@@ -1,7 +1,7 @@
 module.exports.help=function help()
 {console.log(`List of all the commands
-                1. tpp view src -t
-                2. tpp view src -f
-                3. tpp untreefy src dest
-                4. tpp treefy src dest`);
+                1. node compDecomp view src -t
+                2. node compDecomp view src -f
+                3. node compDecomp untreefy src dest
+                4. node compDecomp treefy src dest`);
 }
